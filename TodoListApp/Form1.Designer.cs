3d65bca6593e9f9ade549487c0d505bac6a1003a
tahacan.atak <tahacan.atak@gmail.com>
2019-10-02 20:40:10 +0300

    Add project files.

---
+++ TodoListApp/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TodoListApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clbToDos = new System.Windows.Forms.CheckedListBox();
+            this.txtToDo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clbToDos
+            // 
+            this.clbToDos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.clbToDos.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.clbToDos.FormattingEnabled = true;
+            this.clbToDos.Location = new System.Drawing.Point(7, 82);
+            this.clbToDos.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.clbToDos.Name = "clbToDos";
+            this.clbToDos.Size = new System.Drawing.Size(343, 340);
+            this.clbToDos.TabIndex = 0;
+            this.clbToDos.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.clbToDos_ItemCheck);
+            this.clbToDos.KeyDown += new System.Windows.Forms.KeyEventHandler(this.clbToDos_KeyDown);
+            // 
+            // txtToDo
+            // 
+            this.txtToDo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtToDo.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtToDo.Location = new System.Drawing.Point(7, 41);
+            this.txtToDo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtToDo.Name = "txtToDo";
+            this.txtToDo.Size = new System.Drawing.Size(343, 28);
+            this.txtToDo.TabIndex = 1;
+            this.txtToDo.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtToDo_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(5, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Yapılacak:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 438);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtToDo);
+            this.Controls.Add(this.clbToDos);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Yapılacaklar Listesi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox clbToDos;
+        private System.Windows.Forms.TextBox txtToDo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TodoListApp/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TodoListApp
+{
+    public partial class Form1 : Form
+    {
+        TodoListDbEntities db = new TodoListDbEntities();
+        public Form1()
+        {
+            InitializeComponent();
+            ListToDos();
+        }
+
+        private void ListToDos()
+        {
+            clbToDos.Items.Clear();
+            clbToDos.DisplayMember = "Title";
+
+            foreach (TodoItem item in db.TodoItems.OrderByDescending( x => x.Id)) // id  ye göre sıralar
+            {
+                clbToDos.Items.Add(item, item.IsDone); // IsDone Seciliyse secili gelsin
+            }
+        }
+
+        private void txtToDo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string title = txtToDo.Text.Trim();
+
+                if (title == "")
+                    return;
+
+                TodoItem item = new TodoItem { Title = title };
+                db.TodoItems.Add(item);
+                db.SaveChanges(); //değişiklikleri kaydet
+
+                //MessageBox.Show(item.Id.ToString());
+
+                clbToDos.Items.Insert(0, item); // en başa ekler
+
+                e.SuppressKeyPress = true;
+                txtToDo.Clear();
+            }
+        }
+
+        private void clbToDos_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete && clbToDos.SelectedIndex > -1)
+            {
+                TodoItem item = (TodoItem)clbToDos.SelectedItem;
+
+                db.TodoItems.Remove(item);
+                db.SaveChanges();
+
+                clbToDos.Items.Remove(item);
+            }
+        }
+
+        private void clbToDos_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            TodoItem item = (TodoItem)clbToDos.Items[e.Index];
+            item.IsDone = e.NewValue == CheckState.Checked ? true : false;
+            db.SaveChanges(); 
+        }
+    }
+}
